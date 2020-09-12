5d5a84ef02c938c37325e824608a835f9fd408ad
mrbin <mrbin@DESKTOP-L9U540L>
2020-09-12 21:21:58 +0700

    gagaga

--- src/gitHub/HelloWorld.java
+++ src/gitHub/HelloWorld.java
@@ -3,6 +3,6 @@ package gitHub;
 public class HelloWorld {
 	public static void main(String[] args) {
 		// da fix
-		System.out.println("Hello moi nguoi nha hehe");
+		System.out.println("Hello moi nguoi nha hehehe");
 	}
 }
